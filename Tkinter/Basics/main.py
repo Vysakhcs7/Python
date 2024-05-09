a318f7b52e056902ab792ccd10b5da2f15aa5f8d
Vysakh Sudhan <73440271+Vysakhcs7@users.noreply.github.com>
2024-05-09 20:36:20 +0530

    nill

--- Tkinter/Basics/main.py
+++ Tkinter/Basics/main.py
@@ -10,9 +10,14 @@ root.resizable(0,0)
 root.config(bg = '#123456')
 
 name_label_1 = tkinter.Label(root, text = "STM32 ARR Calculator",font=('Arial', 15), bg ='#E6F2FF')
-name_label_1.pack(padx = 4,pady = 4)
-name_button = tkinter.Button(root, text= "Button",bg="#E6F2FF",activebackground="white")
-name_button.pack()
+name_label_1.grid(row = 0, column = 0)
+
+text_box = tkinter.Text(root, height=1, width=20)
+text_box.grid(row=1,column=0)
+
+name_button = tkinter.Button(root, text= "Submit",bg="#E6F2FF",activebackground="green")
+name_button.grid(row=2,column=2)
+
 
 root.mainloop()
 
